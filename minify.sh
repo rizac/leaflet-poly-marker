ecc71e27dceb17cbef7ef5e20db51bce03c7a06b
rizac <riccardo.zaccarelli@gmail.com>
2021-05-14 10:36:52 +0200

    improve minifier

--- minify.sh
+++ minify.sh
@@ -3,5 +3,7 @@
 # terser (npm install terser -g. See https://docs.npmjs.com/cli/v7/commands/npm-install)
 # and setting the node path specifically (see https://stackoverflow.com/a/15849375)
 NODE_PATH="/usr/local/lib/node_modules"
-terser ./polymarker.js > ./polymarker.min.js
+OUTFILE="./polymarker.min.js"
+echo "// (c) R. Zaccarelli (rizac@github.com)" > $OUTFILE
+terser ./polymarker.js >> $OUTFILE
 echo "Minified polymarker.min.js"
