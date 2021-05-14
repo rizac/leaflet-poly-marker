d87e41a69015b9e9ad8c2f3d9fedd00f61aca21c
rizac <riccardo.zaccarelli@gmail.com>
2021-05-14 09:30:23 +0200

    add minifier script using terser

---
+++ minify.sh
@@ -0,0 +1,7 @@
+# Script using terser to minify the javascript. Requirements:
+# npm (brew install npm or apt-get counterpart in Ubuntu)
+# terser (npm install terser -g. See https://docs.npmjs.com/cli/v7/commands/npm-install)
+# and setting the node path specifically (see https://stackoverflow.com/a/15849375)
+NODE_PATH="/usr/local/lib/node_modules"
+terser ./polymarker.js > ./polymarker.min.js
+echo "Minified polymarker.min.js"
